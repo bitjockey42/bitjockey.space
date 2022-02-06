233f7d9f88beb7ecd5fdd28eb0616264567bb533
Bitjockey <fastjack@jackpoint.network>
2022-02-05 21:30:51 -0500

    list psots

--- src/templates/categoryTemplate.js
+++ src/templates/categoryTemplate.js
@@ -1,6 +1,33 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 
-export default function categoryTemplate() {
-    return <h1>Category Here</h1>
+export default function categoryTemplate({ data }) {
+  const { edges } = data.allMdx
+
+  const Notes = edges.map(edge => (
+    <article>
+      <Link to={`/garden/${edge.node.slug}`}>
+        <h1>{edge.node.frontmatter.title}</h1>
+      </Link>
+      <p>{edge.node.frontmatter.date}</p>
+    </article>
+  ))
+  return <section>{Notes}</section>
+
 }
+
+export const query = graphql`
+  query CategoryQuery {
+    allMdx(filter: {slug: {regex: "/blog/"}}) {
+      edges {
+        node {
+          slug
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+`
