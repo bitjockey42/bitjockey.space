96a0bf0ca17b19dacbcddf4029ae447f4f32db4f
Bitjockey <fastjack@jackpoint.network>
2022-02-07 03:32:20 -0500

    Fix link

--- src/components/tableOfContents.js
+++ src/components/tableOfContents.js
@@ -1,14 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
 
 const TableOfContents = ({ mdx }) => {
   return (
     <aside className="menu">
       <p className="menu-label">Table of Contents</p>
       <ul className="menu-list">
-        {mdx.tableOfContents.items.length > 0 && mdx.tableOfContents.items.map((item, i) => (
-          <li key={i}><Link to={`${item.url}`}>{item.title}</Link></li>
-        ))}
+        {mdx.tableOfContents.items.length > 0 &&
+          mdx.tableOfContents.items.map((item, i) => (
+            <li key={i}>
+              <a href={`${item.url}`}>{item.title}</a>
+            </li>
+          ))}
       </ul>
     </aside>
   )
